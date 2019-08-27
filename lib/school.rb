b84af5302fdd48826077b5e3e865a9d5c1c5d984
jw20191n <clairejw1015@gmail.com>
2019-08-27 16:56:30 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
 # code here!
+# require 'pry'
+
+class School
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = { }
+    end
+
+    def roster
+        @roster 
+    end
+
+    def add_student(name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = [ ]
+            @roster[grade] << name
+        end
+
+        @roster
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do | key, value |
+            @roster[key] = @roster[key].sort
+        end
+
+        @roster
+    end
+        
+end
+
+school = School.new("Bayside High School")
+
