d25e0c374544e26f5d8a5474d40dd990f339aebb
github username <github email address>
2019-08-27 17:08:45 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+require "pry"
+
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade] << name 
+        else 
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end 
+  #  binding.pry
+
+    def sort
+        @roster.each do | key, value |
+            value.sort!
+        end
+    end
+end
+
