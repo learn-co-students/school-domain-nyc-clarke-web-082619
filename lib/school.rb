46ee92a8bacfa744ba120617a39a00be9ebfe507
github username <github email address>
2019-08-27 17:48:42 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require "pry"
+class School
+    attr_reader :roster, :name
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        roster[grade] ? roster[grade] << student : roster[grade] = [student]
+    end 
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.reduce({}) do |memo, (grade, students)| 
+            memo[grade] = roster[grade].sort
+            memo
+        end
+    end
+   
+end
