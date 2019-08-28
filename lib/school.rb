1ccd09ae57e7c0fd1189b16cd6af967824f7a4af
github username <github email address>
2019-08-28 12:36:46 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster 
+        @roster
+    end
+
+    def add_student(student_name, grade)
+        if roster[grade]
+            @roster[grade] << student_name
+        else
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade) 
+        @roster[grade]
+    end
+
+    def sort
+        hash = {}
+        @roster = @roster.each {|key,value| hash[key] = value.sort}
+        @roster = hash        
+    end
+end
